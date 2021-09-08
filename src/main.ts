f872472580a7c52e07886e96356ce49b0453950b
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2021-09-08 09:20:13 -0700

    oatv bouncing around

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import "./style.css";
 import * as PIXI from "pixi.js";
+
+// array of oatvs
+const oatvArray: Array<MovingSprite> = [];
 const app: PIXI.Application = new PIXI.Application({
   resizeTo: window,
   width: window.innerWidth,
@@ -10,33 +13,54 @@ const app: PIXI.Application = new PIXI.Application({
 });
 
 const loader: PIXI.Loader = PIXI.Loader.shared;
-const oatv_sprite: PIXI.Sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
-const oatv_sprite2: PIXI.Sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
 loader.add("/texture.png").load(() => {
   const texture: PIXI.Texture<PIXI.Resource> = loader.resources["/texture.png"].texture!;
-  console.log(texture);
-  // const sheet: PIXI.Spritesheet = loader.resources["/texture.png"].spritesheet!;
-  // oatv_sprite.texture = sheet.textures["oatv-logo.png"];
-  oatv_sprite.texture = texture;
+  const oatv_sprite: PIXI.Sprite = new PIXI.Sprite(texture);
   oatv_sprite.anchor.set(0.5);
-  oatv_sprite.x = app.view.width / 2;
-  oatv_sprite.y = app.view.height / 2;
+  oatv_sprite.x = Math.random() * (app.view.width - 260) + 130;
+  oatv_sprite.y = Math.random() * (app.view.height - 260) + 130;
 
   app.stage.addChild(oatv_sprite);
+  oatvArray.push(new MovingSprite(oatv_sprite));
 });
 
-console.log(app.view.width, oatv_sprite.width);
 app.ticker.add((delta) => {
-  const { width, height } = oatv_sprite.texture.frame;
-  if (oatv_sprite.x >= app.view.width - width / 2) {
-    // console.log(width);
-    console.log(oatv_sprite.x, app.view.width - width / 2);
-    return;
-  }
-  oatv_sprite.x += 2.5;
+  oatvArray.forEach((oatv) => {
+    oatv.update();
+  });
 });
 
 /**
  * Resizing function
  */
 window.addEventListener("resize", () => {});
+
+class MovingSprite {
+  sprite: PIXI.Sprite;
+  dx: number;
+  dy: number;
+
+  constructor(sprite: PIXI.Sprite) {
+    this.sprite = sprite;
+    this.dx = Math.random() * 3 * Math.sign(Math.random() > 0.5 ? 1 : -1);
+    this.dy = Math.random() * 3 * Math.sign(Math.random() > 0.5 ? 1 : -1);
+  }
+
+  update() {
+    const { width, height } = this.sprite.texture.frame;
+    if (this.sprite.x >= app.view.width - width / 2 || this.sprite.x <= 0 + width / 2) {
+      this.dx = -this.dx;
+    }
+
+    if (this.sprite.y <= 0 + height / 2 || this.sprite.y >= app.view.height - height / 2) {
+      this.dy = -this.dy;
+    }
+
+    this.animate();
+  }
+
+  animate() {
+    this.sprite.x += this.dx;
+    this.sprite.y += this.dy;
+  }
+}
