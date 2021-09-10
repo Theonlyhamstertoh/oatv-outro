d8fdd220048f918e2ebd908fc1a9d61b448e5a52
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2021-09-10 09:07:59 -0700

    organize variables into classes for better organization

--- src/main.ts
+++ src/main.ts
@@ -9,98 +9,124 @@ import randomColor from "randomcolor";
 // import { OldFilmFilter } from "@pixi/filter-old-film";
 // color array
 
-export const app: PIXI.Application = new PIXI.Application({
-  resizeTo: window,
-  width: window.innerWidth,
-  backgroundColor: 0x22222,
-  antialias: true,
-  height: window.innerHeight,
-  autoDensity: true,
-  view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-});
-
 /**
  *
  * Old Film Filter
  *
  */
-
 class BackgroundOldFilm {
   oldFilm: OldFilmFilter;
   background: PIXI.Sprite;
   container: PIXI.Container;
-
-  constructor() {
+  constructor(app: PIXI.Application) {
     this.oldFilm = new OldFilmFilter();
     this.background = new PIXI.Sprite();
+    this.background.width = window.innerWidth;
+    this.background.height = window.innerHeight;
+    this.background.anchor.set(0.5);
+    this.background.x = app.view.width / 2;
+    this.background.y = app.view.height / 2;
+
     this.container = new PIXI.Container();
 
-    this.oldFilm.sepia = 0.4;
-    this.oldFilm.noise = 0.2;
+    this.oldFilm.sepia = 0;
+    this.oldFilm.autoFit = true;
+    this.oldFilm.noise = 0.1;
     this.oldFilm.scratch = 0.7;
     this.oldFilm.scratchDensity = 0.5;
 
-    app.stage.addChild(this.background);
-    // this.container.addChild(this.background);
-    // app.stage.addChild(this.container);
-    // this.container.filters = [this.oldFilm];
-    app.stage.filters = [this.oldFilm];
-
-    // this.background.tint = 0xb38c38;
     this.background.alpha = 0.5;
+
+    app.stage.addChild(this.background);
+    app.stage.filters = [this.oldFilm];
+    // app.stage.addChild(this.container)
+    app.ticker.add((delta) => {
+      this.oldFilm.seed += 0.01 * delta;
+      // this.oldFilm.seed > 4 && (this.oldFilm.seed = 0);
+    });
   }
+
+  initializeBackground() {}
 }
 
 /**
- * Filters
+ *
+ * Controller Class
+ *
  */
-// array of oatvs
-export const oatvArray: Array<MovingSprite> = [];
-const totalOATVCount = 5;
+class Controller {
+  app: PIXI.Application;
+  oatvArray: Array<MovingSprite>;
+  scaleFactor: number;
+  count: number;
+  colorMatrixFilter: any; // suppose to be PIXI.filters.ColorMatrixFilter()
+  oldFilmTexture: BackgroundOldFilm;
+  oatvContainer: PIXI.Container;
+
+  constructor() {
+    this.app = new PIXI.Application({
+      resizeTo: window,
+      width: window.innerWidth,
+      backgroundColor: 0x22222,
+      antialias: true,
+      height: window.innerHeight,
+      autoDensity: true,
+      view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+    });
+
+    this.count = 5;
+    this.oatvArray = [];
+    this.scaleFactor = 0.5;
+    this.oldFilmTexture = new BackgroundOldFilm(this.app);
+    this.oatvContainer = new PIXI.Container();
+    this.app.stage.addChild(this.oatvContainer);
+
+    this.colorMatrixFilter = new PIXI.filters.ColorMatrixFilter();
+    this.oatvContainer.filters = [this.colorMatrixFilter];
+    this.colorMatrixFilter.brightness(1.7, true);
+
+    this.resizeListener();
+    this.app.ticker.add(() => {
+      this.oatvArray.forEach((oatv) => {
+        oatv.update();
+      });
+    });
+  }
+
+  resizeListener() {
+    window.addEventListener("resize", () => {
+      controller.oatvArray.forEach(() => {});
+    });
+  }
+}
+const controller = new Controller();
+
 const loader: PIXI.Loader = PIXI.Loader.shared;
-const oatvContainer = new PIXI.Container();
-const scaleFactor = 0.5;
 
-const colorMatrixFilter = new PIXI.filters.ColorMatrixFilter();
-
-const filmTexture = new BackgroundOldFilm();
-app.stage.addChild(oatvContainer);
+// const filmTexture = new BackgroundOldFilm();
 loader.add(["/oatv-logo.png", "/grunge.jpg"]).load(() => {
   const texture: PIXI.Texture<PIXI.Resource> = loader.resources["/oatv-logo.png"].texture!;
   const grungeTexture: PIXI.Texture<PIXI.Resource> = loader.resources["/grunge.jpg"].texture!;
-  filmTexture.background.texture = grungeTexture;
-  for (let i = 0; i < totalOATVCount; i++) {
+  controller.oldFilmTexture.background.texture = grungeTexture;
+
+  for (let i = 0; i < controller.count; i++) {
     const oatv_sprite: PIXI.Sprite = new PIXI.Sprite(texture);
     oatv_sprite.anchor.set(0.5);
-    oatv_sprite.scale.set(scaleFactor);
+    oatv_sprite.scale.set(controller.scaleFactor);
 
-    oatv_sprite.x = Math.random() * (app.view.width - oatv_sprite.width) + oatv_sprite.width / 2;
-    oatv_sprite.y = Math.random() * (app.view.height - oatv_sprite.height) + oatv_sprite.height / 2;
+    oatv_sprite.x =
+      Math.random() * (controller.app.view.width - oatv_sprite.width) + oatv_sprite.width / 2;
+    oatv_sprite.y =
+      Math.random() * (controller.app.view.height - oatv_sprite.height) + oatv_sprite.height / 2;
 
-    oatvContainer.addChild(oatv_sprite);
-    oatvArray.push(new MovingSprite(oatv_sprite, scaleFactor));
+    controller.oatvContainer.addChild(oatv_sprite);
+    controller.oatvArray.push(new MovingSprite(oatv_sprite, controller.scaleFactor));
   }
-  oatvContainer.filters = [colorMatrixFilter];
-  colorMatrixFilter.brightness(2, true);
-});
-
-app.ticker.add(() => {
-  filmTexture.oldFilm.seed += 0.01;
-
-  oatvArray.forEach((oatv) => {
-    oatv.update();
-  });
 });
 
 /**
  * Resizing function
  */
-window.addEventListener("resize", () => {
-  oatvArray.forEach(() => {
-    // oatv.sprite.scale.set(0.3);
-    // oatv.update();
-  });
-});
 
 // function distance(a: PIXI.Sprite, b: PIXI.Sprite) {
 //   const xDistance: number = b.x - a.x;
@@ -142,7 +168,7 @@ export default class MovingSprite {
     const { width, height } = this.sprite.texture.frame;
 
     if (
-      this.sprite.x >= app.view.width - (width * this.scaleFactor) / 2 ||
+      this.sprite.x >= controller.app.view.width - (width * this.scaleFactor) / 2 ||
       this.sprite.x <= 0 + (width * this.scaleFactor) / 2
     ) {
       this.velocity.x = -this.velocity.x;
@@ -152,7 +178,7 @@ export default class MovingSprite {
 
     if (
       this.sprite.y <= 0 + (height * this.scaleFactor) / 2 ||
-      this.sprite.y >= app.view.height - (height * this.scaleFactor) / 2
+      this.sprite.y >= controller.app.view.height - (height * this.scaleFactor) / 2
     ) {
       this.velocity.y = -this.velocity.y;
       this.sprite.tint = this.generateColor();
@@ -174,7 +200,7 @@ export default class MovingSprite {
     return numberColor;
   }
   detectCollision() {
-    oatvArray.forEach((oatv) => {
+    controller.oatvArray.forEach((oatv) => {
       if (oatv.key === this.key) return;
       const xSpaceBetween: number = oatv.sprite.width / 2 + this.sprite.width / 2;
       const ySpaceBetween = oatv.sprite.height / 2 + this.sprite.height / 2;
