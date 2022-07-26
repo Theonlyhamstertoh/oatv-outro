f0299248a2465d8053e9e59332b97a26d5281fae
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-07-26 12:06:57 -0700

    speed up

--- src/MovingSprite.ts
+++ src/MovingSprite.ts
@@ -27,8 +27,8 @@ export default class MovingSprite {
     this.scaleFactor = scaleFactor;
     this.mass = 1 * scaleFactor;
     this.velocity = {
-      x: Math.random() - 0.5 + 2,
-      y: Math.random() - 0.5 + 2,
+      x: Math.random() - 0.5 + 4,
+      y: Math.random() - 0.5 + 4,
     };
     this.key = uuidv4();
   }
